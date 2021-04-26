0e855ab1f406fc0c0cb1f29f66c1adb7806880f5
Anurag <anumaurya114@gmail.com>
2021-04-27 00:51:16 +0530

    updated main.py

--- main.py
+++ main.py
@@ -19,16 +19,16 @@ prereq_score = {
 }
 
 quizz_details = {
-    "filename": 'E://school//template questions//questionTemplateGenerator//output//CBSE-maths-ch-4.csv',
+    "filename": r"E:\school\algo_playground_experiments\algo-playground\questions\CBSE-maths-class-6-ch-4.csv",
     "topic":"all"
 }
 quizz_handler = QuizzHandler(quizz_details, last_quzz_detail, prereq_score)
 quizz_handler.load_quizz()
-
+prev_question = None
 
 @app.route('/')
 def start():
-    return '<h1>Quizz </h1>  <form action="/quizz"> <input type="submit" value="start" > </form>'
+    return '<h1>Quizz </h1>  <form action="/next_question"> <input type="submit" value="start" > </form>'
 
 def process_base64(string):
     if string=='' or pd.isnull(string):
@@ -36,15 +36,35 @@ def process_base64(string):
     image = (string[2:-1])
     return image
 
-@app.route('/quizz')
-def quiz():
- question = quizz_handler.question()
- 
- if question=={}:
-     return start() 
- image = process_base64(question['Encoded_img'])
- 
- return render_template('main.html', q = question,myimage=image)
+def get_user_answer(form):
+    keys = list(form.keys())
+    if len(keys)==1:
+        return list(form.keys())[0]
+    return None
+
+
+@app.route('/next_question',methods=["GET","POST"])
+def next_question():
+    global prev_question
+    if request.method=='GET':
+        question = quizz_handler.next_question()
+        image = process_base64(question['Encoded_img'])
+        return render_template('main.html', q = question,myimage=image)
+
+    user_answer = get_user_answer(request.form)
+    quizz_handler.submit_answer(user_answer)
+    
+
+    question = quizz_handler.next_question()
+    if question=={}:
+        return start() 
+    image = process_base64(question['Encoded_img'])
+    return render_template('main.html', q = question,myimage=image)
+
+# @app.route('/question_submit',methods=["POST"])
+# def quiz(request):
+#     print(request.form)
+#     next_question()
 
 
 # @app.route('/quiz', methods=['POST'])
@@ -57,4 +77,4 @@ def quiz():
 #  return '<h1>Correct Answers: <u>'+str(correct)+'</u></h1>'
 
 if __name__ == '__main__':
- app.run(debug=True)
+    app.run(debug=True)
--- util.py
+++ util.py
@@ -8,6 +8,8 @@ class QuizzHandler:
         self.last_quzz_detail = last_quzz_detail
         self.prereq_score = prereq_score
         self.question_count = 0
+        self.curr_question = None
+        self.question_answer_dict = {}
     
     def load_quizz(self):
         self.all_qs = pd.read_csv(self.quizz_file, sep="|")
@@ -22,13 +24,31 @@ class QuizzHandler:
 
 
     
-    def question(self):
+    def next_question(self):
         if self.check_termination():
             return {}
         index = random.randint(0,len(self.all_qs)-1)
         question = self.all_qs.iloc[index].to_dict()
         self.question_count+=1
+        self.curr_question = question
         return question
+    
+    def check_answer(self,user_answer):
+        if self.curr_question['Answer'].lower()==user_answer.lower():
+            return True
+        return False
+    
+    def submit_answer(self,user_answer):
+        #if skipped i.e. user_answer==None
+        if user_answer==None:
+            #skipped the question
+            pass
+        else:
+            #check answer and update algorithm
+            pass
+    
+    def get_curr_question(self):
+        return self.curr_question
     
     def check_termination(self):
         if self.question_count>12:
@@ -37,5 +57,3 @@ class QuizzHandler:
         pass
 
 
-    
-    
