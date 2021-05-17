3a7188ccff406cde4daad93b2c758a8f6de5987c
rajanking64 <krsingh2004@gmail.com>
2021-05-17 13:59:28 +0900

    Added Questions Traverse functionality

--- util.py
+++ util.py
@@ -44,7 +44,7 @@ class StudentQuizDetail:
 
 
 
-def filter_ques(questions,type_=None,level=None):
+def filter_ques(questions,type_=None,level=None, positiveFlag=True):
     if (type_== None) and (level == None):
         return questions
     filtered = questions 
@@ -54,41 +54,66 @@ def filter_ques(questions,type_=None,level=None):
         filtered = list(filter(lambda x: x['Level'] == level, filtered))
     if filtered:
         return filtered
+    # else:
+    #     return questions
     else:
-        return questions
+        jump = 1 if positiveFlag else -1
+        next_question_Type, next_question_Level = getNextTypeLevel(type_,level,jump)
+        filtered = filter_ques(questions,next_question_Type,next_question_Level, positiveFlag)
+    return filtered
+
+def getNextTypeLevel(Type,Level,Jump):
+    level_order = [('C',1),('C',2),('CI',1),('C',3),('CI',2),('CIP',1),('CI',3),('CIP',2),('CIP',3)]
+    curr_index = level_order.index((Type,Level))
+    print("curr_index",curr_index)
+    return level_order[max(min((curr_index+Jump),len(level_order)-1),0)]
 
 class QuizHandler:
     def __init__(self,quiz_details):
         self.class_ = quiz_details['class']
         self.chapter = quiz_details['chapter']
         self.questions = pd.read_csv(quiz_details['filename'],delimiter="|").to_dict('records')
-
-        print(self.questions[0])
     
-    def getNextTypeLevel(self,Type,Level,Jump):
-        level_order = [('C',1),('C',2),('CI',1),('C',3),('CI',2),('CIP',1),('CI',3),('CIP',2),('CIP',3)]
-        curr_index = level_order.index((Type,Level))
+    # def getNextTypeLevel(self,Type,Level,Jump):
+    #     level_order = [('C',1),('C',2),('CI',1),('C',3),('CI',2),('CIP',1),('CI',3),('CIP',2),('CIP',3)]
+    #     curr_index = level_order.index((Type,Level))
 
-        return level_order[max(min((curr_index+Jump),len(level_order)),0)]
+    #     return level_order[max(min((curr_index+Jump),len(level_order)),0)]
 
     def question(self,student_quiz_detail):
         usedIDList = [elem['ID'] for elem in student_quiz_detail.Questions]
         if student_quiz_detail.Type == None:
-            return filter_ques(self.questions,'C',1)[0]
+            return filter_ques(self.questions,'C',1,True)[0]
         else:
+            positiveFlag = True
             if(student_quiz_detail.Questions[-1]['Correct']):
                 if(len(student_quiz_detail.Questions)>1 and student_quiz_detail.Questions[-2]['Correct']):
-                    next_question_Type, next_question_Level = self.getNextTypeLevel(student_quiz_detail.Type, student_quiz_detail.Level,2)
-                    filtered_questions = filter_ques(self.questions,next_question_Type, next_question_Level)
-                    return [ elem for elem in filtered_questions if elem['ID'] not in usedIDList][0]
+                    next_question_Type, next_question_Level = getNextTypeLevel(student_quiz_detail.Type, student_quiz_detail.Level,2)
+                    print("xxx:",next_question_Type, next_question_Level)
                 else:            
-                    next_question_Type, next_question_Level = self.getNextTypeLevel(student_quiz_detail.Type, student_quiz_detail.Level,1)
-                    filtered_questions = filter_ques(self.questions,next_question_Type, next_question_Level)
-                    return [ elem for elem in filtered_questions if elem['ID'] not in usedIDList][0]
+                    next_question_Type, next_question_Level = getNextTypeLevel(student_quiz_detail.Type, student_quiz_detail.Level,1)
             else:
-                next_question_Type, next_question_Level = self.getNextTypeLevel(student_quiz_detail.Type, student_quiz_detail.Level,-1)
-                filtered_questions = filter_ques(self.questions,next_question_Type, next_question_Level)
-                return [ elem for elem in filtered_questions if elem['ID'] not in usedIDList][0]
+                positiveFlag = False
+                next_question_Type, next_question_Level = getNextTypeLevel(student_quiz_detail.Type, student_quiz_detail.Level,-1)
+
+            filtered_questions = filter_ques(self.questions,next_question_Type,next_question_Level,positiveFlag)
+            # print(next_question_Type,next_question_Level)
+            # while(len(filtered_questions)==0):
+            #     # jump = 1 if positiveFlag else -1
+            #     # print("jump: ",jump)
+            #     # print("previous",next_question_Type,next_question_Level)
+            #     # next_question_Type, next_question_Level = self.getNextTypeLevel(next_question_Type,next_question_Level,jump)
+            #     # filtered_questions = filter_ques(self.questions,next_question_Type,next_question_Level)
+            #     # print("next",next_question_Type,next_question_Level)
+            #     filtered_questions = filter_ques(self.questions,next_question_Type,next_question_Level,positiveFlag)
+            candidateQuestions = [elem for elem in filtered_questions if elem['ID'] not in usedIDList]
+            while(len(candidateQuestions)==0):
+                jump = 1 if positiveFlag else -1
+                next_question_Type, next_question_Level = getNextTypeLevel(next_question_Type,next_question_Level,jump)
+                filtered_questions = filter_ques(self.questions,next_question_Type,next_question_Level,positiveFlag)
+                candidateQuestions = [elem for elem in filtered_questions if elem['ID'] not in usedIDList]
+            return candidateQuestions[0]
+            # return [elem for elem in filtered_questions if elem['ID'] not in usedIDList][0]
         # elif student_quiz_detail.Type == 'C':
         #     return filter_ques(self.questions,'CI',1)[0]
         # elif student_quiz_detail.Type == 'CI':
